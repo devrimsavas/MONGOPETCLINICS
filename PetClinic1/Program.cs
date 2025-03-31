8cc95f8753ba5c4128f20bbc542e0e8df457bbdd
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2025-03-31 21:46:08 +0200

    swagger comments

--- PetClinic1/Program.cs
+++ PetClinic1/Program.cs
@@ -1,11 +1,36 @@
 using PetClinic1.Services;
 using PetClinic1.Models;
-using Microsoft.AspNetCore.Mvc; //optional since it is minimalist program
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models; //optional since it is minimalist program
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 //register MongoDbService
 builder.Services.AddSingleton<MongoDbService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Mongo Pet Hospital",
+        Description = "Pet Hospital"
+    });
+    //enable xml
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
 
 app.MapGet("/", () => "Hello World!");
 
@@ -13,10 +38,16 @@ app.MapGet("/", () => "Hello World!");
 //add an end-point to test our database 
 
 //get all
-app.MapGet("/pets", async (MongoDbService db)=> {
-    var pets=await db.GetAllPetsAsync();
+
+
+app.MapGet("/pets", async (MongoDbService db) =>
+{
+    var pets = await db.GetAllPetsAsync();
     return Results.Ok(pets);
-});
+})
+.WithName("GetAllPets")
+.WithSummary("Returns all pets in the system")
+.WithDescription("Fetches pet records");
 
 //add new pet
 //add [frombody ] optional since it is minimal program 
